057df16357eb2e99fd095cd3924d9dbf123fb125
tebii <kunderscore1tty@gmail.com>
2023-10-13 00:06:57 +0800

    Add computerChoice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello world!");
+console.log(getComputerChoice());
+function getComputerChoice(){
+  let choiceNum = Math.floor(Math.random() * 3) + 1;
+  let computerChoice;
+  switch(choiceNum) {
+     case 1:
+    computerChoice = "rock";
+      break;
+    case 2:
+    computerChoice = "paper";
+      break;
+    case 3:
+    computerChoice= "scissors";
+      break;
+  }
+  return computerChoice;
+}
+  
+
